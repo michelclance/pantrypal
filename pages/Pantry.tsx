a0a5f34eec1a906036b7c4ab873051a71e702992
michelclance <michel.clance@gmail.com>
2023-03-09 17:24:18 -0800

    update to mobile css

--- pages/Pantry.tsx
+++ pages/Pantry.tsx
@@ -6,12 +6,15 @@ import Sidebar from '../components/sidebar';
 const Pantry: React.FC = () => {
   return (
     <div>
-      
       <FormContextProvider>
-      <div className="flex flex-wrap" >
-      <Sidebar />
-      <Form />
-    </div>
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/4">
+            <Sidebar />
+          </div>
+          <div className="w-full md:w-3/4">
+            <Form />
+          </div>
+        </div>
       </FormContextProvider>
     </div>
   );
